cdfb71b6ac2f3314ae09973667093db675eeb7e8
Frederik k <frederikkarsum@yahoo.dk>
2025-07-08 19:06:06 +0200

    added initial stock class

--- StockSimulator/src/stock.java
+++ StockSimulator/src/stock.java
@@ -1,11 +1,23 @@
 public class stock {
-    String symbol;
-    Double marketPrice;
-    String companyName;
+    private final String symbol;
+    private Double marketPrice;
+    private final String companyName;
 
     public stock(String symbol, Double marketPrice, String companyName) {
         this.symbol = symbol;
         this.marketPrice = marketPrice;
         this.companyName = companyName;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public double getMarketPrice() {
+        return marketPrice;
+    }
+
+    public String getCompanyName() {
+        return companyName;
     }
 }

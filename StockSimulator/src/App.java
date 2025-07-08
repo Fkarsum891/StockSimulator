56e9ba899567e0c16e371433323e7669ac0720de
Frederik k <frederikkarsum@yahoo.dk>
2025-07-08 18:39:44 +0200

    first commit

--- StockSimulator/src/App.java
+++ StockSimulator/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        stock s = new stock("AAPL", 30.69, "Apple");
     }
 }
--- StockSimulator/src/stock.java
+++ StockSimulator/src/stock.java
@@ -1,3 +1,11 @@
 public class stock {
-    
+    String symbol;
+    Double marketPrice;
+    String companyName;
+
+    public stock(String symbol, Double marketPrice, String companyName) {
+        this.symbol = symbol;
+        this.marketPrice = marketPrice;
+        this.companyName = companyName;
+    }
 }
